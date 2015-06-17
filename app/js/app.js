e485fac52cf23491f7bedde45c9949df69b0b2bf
dibya <dibya.s.mohanty@skf.com>
2015-06-17 17:34:51 +0200

    Added user and admin html

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('coffeeApp', []);
 
 app.controller('mainController', function($scope){
          $scope.userData = [
-            {username: "Verruckt Folger", numOfCups: 2, balance: 0.50},
+            {username: "Johannes Folger", numOfCups: 2, balance: 0.50},
             {username: "John Doe", numOfCups: 3, balance: 0.75}
         ];
 
